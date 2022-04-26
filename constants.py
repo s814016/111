69efd81d2fea447154fa09c084fd267f8017584a
s814016 <90367223+s814016@users.noreply.github.com>
2022-04-26 09:00:23 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -22,11 +22,11 @@ from pygame.locals import *
 
 # Configuration of building shape block
 # Width of the shape block
-BWIDTH     = 20
+BWIDTH     = 30
 # Height of the shape block
-BHEIGHT    = 20
+BHEIGHT    = 30
 # Width of the line around the block
-MESH_WIDTH = 1
+MESH_WIDTH = 3
 
 # Configuration of the player board
 # Board line height
@@ -39,11 +39,11 @@ BOARD_MARGIN     = 2
 # Color declarations in the RGB notation
 WHITE    = (255,255,255)
 RED      = (255,0,0)
-GREEN    = (0,255,0)
-BLUE     = (0,0,255)
+GREEN    = (0,128,0)
+BLUE     = (0,0,128)
 ORANGE   = (255,69,0)
 GOLD     = (255,125,0)
-PURPLE   = (128,0,128)
+PURPLE   = (255,0,255)
 CYAN     = (0,255,255) 
 BLACK    = (0,0,0)
 
